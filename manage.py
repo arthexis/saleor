5fbce7f3543ea00adbb7f75da2d49532195db521
Rafael Guillén <arthexis@gmail.com>
2019-11-15 18:34:31 -0600

    Use dotenv in dev

--- manage.py
+++ manage.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import os
 import sys
+import dotenv
+
 
 if __name__ == "__main__":
+    dotenv.read_dotenv()
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saleor.settings")
-
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
